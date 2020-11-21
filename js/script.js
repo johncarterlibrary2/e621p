4f5e8b4606b4de504d120e6b8f9a5961ca847eee
SilentDeath1 <SilentDeath1@users.noreply.github.com>
2020-11-21 00:14:36 +0000

    Adds required _client user agent
    Adds the required _client useragent within the http request as specified [here](https://e621.net/help/api#endpoints).

--- js/script.js
+++ js/script.js
@@ -763,7 +763,7 @@ $(function () {
             //rating = rating+"+rating:q";
         }
 
-        var jsonUrl = "https://e621.net/posts.json?tags="+e621pRating+"+"+e621pTags +"&limit="+ e621pLimit+`&page=${e621pDescending ? "b": "a"}`+e621pAfterId;
+        var jsonUrl = "https://e621.net/posts.json?tags="+e621pRating+"+"+e621pTags +"&limit="+ e621pLimit+`&page=${e621pDescending ? "b": "a"}`+e621pAfterId+"&_client=E621p Slideshow (GitHub) - github.com/SilentDeath1/e621p/";
         //console.log(jsonUrl);
         //log(jsonUrl);
         var failedAjax = function (data) {
